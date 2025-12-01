e7814cd78bce6753811bc9108de26c79db4f7534
nedas-sim <nedas.sim@gmail.com>
2025-12-01 22:37:08 +0200

    Solve 1.1

---
+++ AdventOfCode25/AdventOfCode25.Solutions/Day01/Models/DialTurn.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode25.Solutions.Day01.Models;
+
+public class DialTurn
+{
+    private DialRotation _rotation;
+    private int _turns;
+
+    public DialTurn(string inputLine)
+    {
+        _rotation = inputLine[0] switch
+        {
+            'L' => DialRotation.Left,
+            'R' => DialRotation.Right,
+            _ => throw new UnreachableException($"Could not rotation direction from input line '{inputLine}'"),
+        };
+
+        _turns = int.Parse(inputLine[1..]);
+    }
+
+    public int Turn(int currentPoint)
+    {
+        currentPoint += _rotation switch
+        {
+            DialRotation.Left => -_turns,
+            DialRotation.Right => _turns,
+            _ => throw new UnreachableException(),
+        };
+
+        return (currentPoint + 100) % 100;
+    }
+}
+
+public enum DialRotation
+{
+    Left = 1,
+    Right,
+}
---
+++ AdventOfCode25/AdventOfCode25.UnitTests/Day01.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode25.Solutions.Day01;
+using AdventOfCode25.Solutions.Day01.Models;
+
+namespace AdventOfCode25.UnitTests;
+
+public class Day01
+{
+    [Fact]
+    public async Task TestSolution1()
+    {
+        int totalZeros = await Solution1.CalculateTotalZerosAsync("sample");
+        Assert.Equal(3, totalZeros);
+    }
+
+    [Fact]
+    public async Task RunSolution1()
+    {
+        int totalZeros = await Solution1.CalculateTotalZerosAsync("task1");
+        Assert.Equal(980, totalZeros);
+    }
+
+    [Theory]
+    [InlineData("L10", 5, 95)]
+    [InlineData("R10", 95, 5)]
+    [InlineData("R5", 95, 0)]
+    [InlineData("R4", 95, 99)]
+    [InlineData("L4", 0, 96)]
+    public void DialTurn_Turn_ShouldGetExpected(string inputLine, int currentValue, int nextExpectedValue)
+    {
+        // Arrange
+        DialTurn dialturn = new(inputLine);
+
+        // Act
+        int newPointerAt = dialturn.Turn(currentValue);
+
+        // Assert
+        Assert.Equal(nextExpectedValue, newPointerAt);
+    }
+}
